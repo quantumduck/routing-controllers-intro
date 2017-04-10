8c597c45ffc3439c54f9a0261caaf9a00da27717
quantumduck <quantum.duck@hotmail.com>
2017-04-10 13:13:58 -0400

    adding notice and redirect in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
   def welcome
     # render :about
@@ -10,6 +11,19 @@ class PagesController < ApplicationController
   end
 
   def kitten
+  end
+
+  def kittens
+  end
+
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
+  end
+
+private#########################################################################
+
+  def set_kitten_url
     requested_size = params[:size]
     @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
+  get '/contest' => 'pages#contest'
 
   get '/kitten/:size' => 'pages#kitten'
+  get '/kittens/:size' => 'pages#kittens'
+
 end
