723a0618bb79d2e1fdefe149f3cec4dcf34081c3
quantumduck <quantum.duck@hotmail.com>
2017-04-10 13:04:42 -0400

    added dynamic kitten route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+
+  def welcome
+    # render :about
+    @header = "This is the welcome page"
+  end
+
+  def about
+    @header = "This is the about us page"
+  end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
 end
